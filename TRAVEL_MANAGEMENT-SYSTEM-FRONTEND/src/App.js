0d98cec01d82ca41eda828e99001f863cd11a3bb
kaweesha-madusanka <kavisadu@gmail.com>
2022-04-03 10:43:23 +0530

    equipment frontend

--- TRAVEL_MANAGEMENT-SYSTEM-FRONTEND/src/App.js
+++ TRAVEL_MANAGEMENT-SYSTEM-FRONTEND/src/App.js
@@ -1,10 +1,31 @@
-import './App.css';
+import EditEquipment from './components/Travel_Equipments/EditEquipment';
+import AddEquipment from './components/Travel_Equipments/AddEquipment';
+import AdminEquipment from './components/Travel_Equipments/AdminEquipment';
+import UserEquipment from './components/Travel_Equipments/UserEquipment';
+import EquipmentReport from './components/Travel_Equipments/EquipmentReport'
+import {BrowserRouter as Router, Route} from "react-router-dom"
+
+
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+    <div> 
+    
+ 
       
+      
+      <Route path ="/equipment/add" exact component={AddEquipment}/>
+      <Route path ="/equipment/edit/:id" exact component={EditEquipment}/>
+      <Route path ="/equipment/admin" exact component={AdminEquipment}/>
+      <Route path ="/equipment" exact component={UserEquipment}/>
+      <Route path ="/equipment/report" exact component={EquipmentReport}/>
+      
+  
     </div>
+    </Router>
+    
+    
   );
 }
 
